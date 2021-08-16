9c75dd9d41e1952bcc4ef3ea6e14b44185d374d4
emil41m5 <emil41m5@CV-BB-5340>
2021-08-16 10:32:06 +0200

    Add project files.

---
+++ EX03ConsolePlay/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EX03ConsolePlay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WindowHeight = 50;
+            Console.WindowWidth = 200;
+            Console.Beep(200, 1000);
+        }
+    }
+}
